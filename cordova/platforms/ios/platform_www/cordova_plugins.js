373503150a8218bb4e6f145e08b76f8a326bc9dd
lukesargeant <lukesarge@gmail.com>
2016-01-31 22:17:57 +0000

    Added cordova powermanagement plugin

--- cordova/platforms/ios/platform_www/cordova_plugins.js
+++ cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-powermanagement/www/powermanagement.js",
+        "id": "cordova-plugin-powermanagement.powermanagement",
+        "pluginId": "cordova-plugin-powermanagement",
+        "clobbers": [
+            "window.powermanagement"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-powermanagement": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
